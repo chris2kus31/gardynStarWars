3981089d760f9bbe2651cf2f38cff92b5ab54ee0
chuy <chris2kus31@gmail.com>
2022-05-06 10:39:26 -0500

    unused code

--- src/components/StarWarsToolTip.tsx
+++ src/components/StarWarsToolTip.tsx
@@ -5,7 +5,7 @@ import "../App.css";
 
 const StarWarsToolTip = (props: ITooltipParams & { color: string }) => {
 
-  const d = useMemo( () => props.value.results,[] ) 
+  const d = useMemo( () => props.value.results,[props.value.results] ) 
   return (
       <Card headStyle={{backgroundColor: '#C7CED4'}} title={props.colDef?.headerName}  style={{width: '50%',backgroundColor: '#FFE81F'}}>
         {d.map( (item:any) => {
@@ -14,33 +14,6 @@ const StarWarsToolTip = (props: ITooltipParams & { color: string }) => {
            )
         } )}
       </Card>
-    // <div
-    //   className="custom-tooltip"
-    //   style={{
-    //     backgroundColor: props.color || "white",
-    //     height: "100%",
-    //     zIndex: 1000,
-    //   }}
-    // >
-    //   <p>
-    //     <span>Characters in Movie</span>
-    //   </p>
-    //   <div style={{ paddingLeft: "10px", zIndex: '999999999 !important' }}>
-    //     <List
-    //       size="small"
-    //       dataSource={d}
-    //       renderItem={(item: any) => (
-    //         <List.Item style={{padding: '0px'}}>
-    //           <List.Item.Meta style={{fontSize: '8px'}} title={item.name} />
-    //         </List.Item>
-    //       )}
-    //     />
-    //     {/* {d.map((item:any) => {
-    //              return <span key={item.id}>{item.name}</span>
-    //           } )} */}
-    //   </div>
-    //   <ReactTooltip />
-    // </div>
   );
 };
 
