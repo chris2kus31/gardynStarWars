d1a8f27ec3ac28c9001043471c517cd3a0b9cb32
chuy <chris2kus31@gmail.com>
2022-05-06 10:35:21 -0500

    performance

--- src/components/StarWarsToolTip.tsx
+++ src/components/StarWarsToolTip.tsx
@@ -1,12 +1,11 @@
 import React, { useMemo } from "react";
 import { List, Tooltip, Tag, Card } from "antd";
-import ReactTooltip from "react-tooltip";
 import { ITooltipParams } from "ag-grid-community";
 import "../App.css";
 
 const StarWarsToolTip = (props: ITooltipParams & { color: string }) => {
 
-  const d = props.value.results;
+  const d = useMemo( () => props.value.results,[] ) 
   return (
       <Card headStyle={{backgroundColor: '#C7CED4'}} title={props.colDef?.headerName}  style={{width: '50%',backgroundColor: '#FFE81F'}}>
         {d.map( (item:any) => {
