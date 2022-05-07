7e4aaf6831a3d54db840b1fb182d0f064ffb8419
chuy <chris2kus31@gmail.com>
2022-05-07 03:34:10 -0500

    refactoring

--- src/components/StarWarsToolTip.tsx
+++ src/components/StarWarsToolTip.tsx
@@ -4,19 +4,24 @@ import { ITooltipParams } from "ag-grid-community";
 import "../App.css";
 
 type ToolTipProps = {
-    colorTag: string
-}
+  colorTag: string;
+};
 
 const StarWarsToolTip = (props: ITooltipParams & ToolTipProps) => {
-
-  const toolTipData = useMemo( () => props.value.results,[props.value.results] ) 
+  const toolTipData = useMemo(() => props.value.results, [props.value.results]);
 
   return (
-      <Card headStyle={{backgroundColor: '#C7CED4'}} title={props.colDef?.headerName}  style={{width: '50%',backgroundColor: '#FFE81F'}}>
-        {toolTipData.map( (item:any) => 
-            <Tag color="black" key={item.id}>{item.name}</Tag> 
-        )}
-      </Card>
+    <Card
+      headStyle={{ backgroundColor: "#C7CED4" }}
+      title={props.colDef?.headerName}
+      style={{ width: "50%", backgroundColor: "#FFE81F" }}
+    >
+      {toolTipData.map((item: any) => (
+        <Tag color="black" key={item.id}>
+          {item.name}
+        </Tag>
+      ))}
+    </Card>
   );
 };
 
