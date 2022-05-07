175ff07f7b6871431c0e61c55df275d26d56b26e
chuy <chris2kus31@gmail.com>
2022-05-07 03:33:45 -0500

    refactoring

--- src/components/TableHeaderComponent.tsx
+++ src/components/TableHeaderComponent.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
+import React from "react";
 import { Button } from "antd";
 import "../App.css";
 
 type TableHeaderComponentProps = {
-   onClick: () => void;
-   loading?: boolean;
-   headerTitle: string;
-   styles?: React.CSSProperties;
-   buttonIcon: JSX.Element
-}
+  onClick: () => void;
+  loading?: boolean;
+  headerTitle: string;
+  styles?: React.CSSProperties;
+  buttonIcon: JSX.Element;
+};
 
-const TableHeaderComponent = ({loading, onClick, headerTitle, styles, buttonIcon}: TableHeaderComponentProps ) => {
-
-    return (
-            <div style={styles}>
-                <h1>{headerTitle}</h1>
-                <Button onClick={onClick} icon={buttonIcon} >Click To Begin</Button>
-            </div>
-    )
-}
+const TableHeaderComponent = ({
+  loading,
+  onClick,
+  headerTitle,
+  styles,
+  buttonIcon,
+}: TableHeaderComponentProps) => {
+  return (
+    <div style={styles}>
+      <h1>{headerTitle}</h1>
+      <Button onClick={onClick} icon={buttonIcon}>
+        Click To Begin
+      </Button>
+    </div>
+  );
+};
 
 export default TableHeaderComponent;
